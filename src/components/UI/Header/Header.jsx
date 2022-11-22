17d50145b0a216599f6213566f914a61da640ee5
Gregor Zhidok <zhidokGD@gmail.com>
2022-11-23 01:37:21 +0300

    rename

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -5,21 +5,12 @@ const Header = () => {
         <header className={styles.header}>
             <div className={styles.layers}>
                 <div className={styles.layersHeader}>
-                    <div className={styles.layersCaption}>WEBJOX</div>
+                    <div className={styles.layersCaption}>Grigory Zhidok</div>
                     <div className={styles.layersTitle}>Parallax</div>
                 </div>
-                <div
-                    className={`${styles.layer} ${styles.layerBase}`}
-                    style={{ backgroundImage: "url(/img/layer-base.png)" }}
-                ></div>
-                <div
-                    className={`${styles.layer} ${styles.layerMiddle}`}
-                    style={{ backgroundImage: "url(/img/layer-middle.png)" }}
-                ></div>
-                <div
-                    className={`${styles.layer} ${styles.layerFront}`}
-                    style={{ backgroundImage: "url(/img/layer-front.png)" }}
-                ></div>
+                <div className={`${styles.layer} ${styles.layerBase}`} style={{ backgroundImage: "url(/img/layer-base.png)" }}></div>
+                <div className={`${styles.layer} ${styles.layerMiddle}`} style={{ backgroundImage: "url(/img/layer-middle.png)" }}></div>
+                <div className={`${styles.layer} ${styles.layerFront}`} style={{ backgroundImage: "url(/img/layer-front.png)" }}></div>
             </div>
         </header>
     );
